bb78d5d6bdb3cb3b6b5a1f3a939117b4e3e5b3f1
Alex Chheng <alexchheng1998@gmail.com>
2018-12-01 14:11:09 -0800

    Hello UI Message and present assets

---
+++ WinterJam/Assets/Scripts/UIScripts/MessageDisplayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageDisplayer : MonoBehaviour {
+
+    public bool isDisplay;
+    public float testDuration;
+    public Text textElement;
+    public string testString;
+
+	// Use this for initialization
+	void Start () {
+        isDisplay = false;
+	}
+	
+	// TESTING ONLY
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DisplayMessage(testString, testDuration);
+        }
+	}
+    public void DisplayMessage(string message, float duration)
+    {
+        isDisplay = true;
+        StartCoroutine(MessageOnCoro(message, duration));
+    }
+
+    IEnumerator MessageOnCoro(string message, float duration)
+    {
+        textElement.enabled = true;
+        textElement.text = message;
+        yield return new WaitForSeconds(duration);
+        textElement.enabled = false;
+    }
+}
