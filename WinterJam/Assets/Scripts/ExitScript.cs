3728d5a425578958602fb45275e41a2ec6182f9a
khoangl <khoangl@csu.fullerton.edu>
2018-12-01 18:19:18 -0800

    Commit_health_bar

---
+++ WinterJam/Assets/Scripts/ExitScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody != null)
+        {
+            if (other.attachedRigidbody.tag == "Player" && PresentCollectionManager.Instance.PercentValue >= (float).7)
+            {
+                MessageDisplayer.instance.DisplayMessage("You Win!", 3);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
