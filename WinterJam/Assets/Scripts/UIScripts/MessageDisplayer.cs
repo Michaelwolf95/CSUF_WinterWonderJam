d55197b2edeeb5ba88fc554f84e375efd153fdbf
Michaelwolf95 <michaelwolf95@gmail.com>
2018-12-01 16:03:18 -0800

    Level layout and enemy config

--- WinterJam/Assets/Scripts/UIScripts/MessageDisplayer.cs
+++ WinterJam/Assets/Scripts/UIScripts/MessageDisplayer.cs
@@ -2,29 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class MessageDisplayer : MonoBehaviour {
+public class MessageDisplayer : MonoBehaviour
+{
 
-    public bool isDisplay;
-    public float testDuration;
-    public Text textElement;
-    public string testString;
+    public TextMeshProUGUI textElement;
 
-	// Use this for initialization
-	void Start () {
-        isDisplay = false;
-	}
-	
-	// TESTING ONLY
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-        {
-            DisplayMessage(testString, testDuration);
-        }
-	}
+    private void Start()
+    {
+        textElement.enabled = false;
+    }
+
     public void DisplayMessage(string message, float duration)
     {
-        isDisplay = true;
         StartCoroutine(MessageOnCoro(message, duration));
     }
 
