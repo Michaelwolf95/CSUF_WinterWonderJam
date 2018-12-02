09f1c0cd32d97b83db917ca115bc4582e1e68f87
Michaelwolf95 <michaelwolf95@gmail.com>
2018-12-01 21:01:09 -0800

    Enemy Death!

---
+++ WinterJam/Assets/Scripts/ActivateAtVelocity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAtVelocity : MonoBehaviour
+{
+    public GameObject target;
+    public Rigidbody rb;
+    public float activationVelocity = 5f;
+
+	// Use this for initialization
+	void Start () {
+		if(target == null && transform.GetChild(0) != null)
+        {
+            target = transform.GetChild(0).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (rb == null) return;
+
+        target.SetActive(rb.velocity.magnitude > activationVelocity);
+
+	}
+}
---
+++ WinterJam/Assets/Scripts/AddPresentOnDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MichaelWolfGames.DamageSystem;
+using UnityEngine.AI;
+
+public class AddPresentOnDeath : HealthManagerEventListenerBase
+{
+    public int count = 2;
+
+    public Rigidbody rb;
+    public NavMeshAgent agent;
+    protected override void DoOnDeath()
+    {
+        PresentCollectionManager.Instance.AddPresent(count);
+        agent.enabled = false;
+        rb.isKinematic = false;
+    }
+
+    protected override void DoOnRevive()
+    {
+    }
+
+    protected override void DoOnTakeDamage(object sender, Damage.DamageEventArgs damageEventArgs)
+    {
+    }
+
+}
